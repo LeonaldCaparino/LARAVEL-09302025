760ba523a247ce4bb0e72f78da5d88291b468787
camshiieee <beldacamilleshainne8@gmail.com>
2025-09-30 16:26:35 +0800

    initial commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>My Website</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+    </head>
+
+    <body class="bg-[url('https://kpopping.com/documents/cb/2/1200/TWICE-fullPicture(51).webp')] bg-no-repeat bg-cover">
+        <div class="">
+            <nav class="bg-gray-800">
+                    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                        <div class="flex h-16 items-center justify-between">
+                            <div class="flex items-center">
+                                <div class="shrink-0">
+                                    <img class="h-10 w-15" src="https://kpopping.com/documents/52/2/400/TWICE-logoPicture.webp?v=8d85b" alt="Your Company">
+                                </div>
+                                    <div class="hidden md:block">
+                                        <div class="ml-10 flex items-baseline space-x-4">
+                                            <!-- Current: "bg-gray-900 text-nwhite", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                                            <a href="/" class="bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
+                                            <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+                                            <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            <div class="hidden md:block">
+                                <div class="ml-4 flex items-center md:ml-6">
+                                    <button type="button" class="relative rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                                    <span class="absolute -inset-1.5"></span>
+                                    <span class="sr-only">View notifications</span>
+                                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+                                        <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" stroke-linecap="round" stroke-linejoin="round" />
+                                    </svg>
+                                    </button>
+
+                                    <!-- Profile dropdown -->
+                                        <button class="relative flex max-w-xs items-center rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                                            <span class="absolute -inset-1.5"></span>
+                                            <span class="sr-only">Open user menu</span>
+                                            <img class="h-10 w-15 rounded-full" src="https://kpopping.com/documents/52/2/400/TWICE-logoPicture.webp?v=8d85b" alt="">
+                                        </button>
+                                </div>
+                            <div class="-mr-2 flex md:hidden">
+                            <!-- Mobile menu button -->
+                            <button type="button" data-toggle="mobile-menu" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                                <span class="absolute -inset-0.5"></span>
+                                <span class="sr-only">Open main menu</span>
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 in-aria-expanded:hidden">
+                                <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 not-in-aria-expanded:hidden">
+                                <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    </div>
+
+                    <div class="md:hidden" id="mobile-menu">
+                        <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+                            <!-- Current: "bg-gray-900 text-nwhite", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                            <a href="/" class="bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current=""page">Home</a>
+                            <a href="/about" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+                            <a href="/contact" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Contact</a>
+                        </div>
+                    </div>
+                    <div class="border-t border-gray-700 pb-3 pt-4">
+                        <div class="flex items-center px-5">
+                        <div class="flex-shrink-0">
+                            <img class="h-10 w-15 rounded-full" src="https://kpopping.com/documents/52/2/400/TWICE-logoPicture.webp?v=8d85b" alt="">
+                        </div>
+                        <div class="ml-3">
+                            <div class="text-base/5 font-medium text-white">Twice</div>
+                            <div class="text-sm font-medium text-gray-400">twiceoneinmillion.com</div>
+                        </div>
+                        <button type="button" class="relative ml-auto shrink-0 rounded-full p-1 text-gray-400 hover:text-white focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500">
+                            <span class="absolute -inset-1.5"></span>
+                            <span class="sr-only">View notifications</span>
+                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+                            <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </button>
+                    
+                    </div>
+                </div>
+            </nav>
+
+            <header class="bg-white shadow">
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h1 class="text-3xl font-bold tracking-tight text-gray-900">
+                        {{ $heading ?? '' }}
+                    </h1>
+                </div>
+            </header>
+
+            <main>
+                <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    {{ $slot }}
+                </div>
+            </main>
+        </div>
+    </body>
+</html>
