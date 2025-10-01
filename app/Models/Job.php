3ceaaddf705b92298b7afd545d4a2ade72d80d47
camshiieee <beldacamilleshainne8@gmail.com>
2025-10-01 16:47:32 +0800

    laravel a file

---
+++ app/Models/Job.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Arr;
+
+class Job {
+    public static function all(): array 
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'CEO',
+                'salary' => '$80,000',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Choreographer',
+                'salary' => '$60,000',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Artist',
+                'salary' => '$50,000',
+            ],
+        ];
+    }
+
+    public static function find(int $id): ?array
+    {
+        return Arr::first(static::all(), fn($job) => $job['id'] == $id);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => Job::all()
+    ]);
 });
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
 
-Route::get('/contact', function () {
-    return view('contact');
+    abort_if(!$job, 404);
+
+    return view('job', ['job' => $job]);
 });
